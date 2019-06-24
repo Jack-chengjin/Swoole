f686aad0aa8bcfa8aa8dee1205c8019bf4d41982
lichengjin <a13381071993@163.com>
2019-06-24 08:28:40 +0800

    djkjf

--- Swoole/http.php
+++ Swoole/http.php
@@ -5,7 +5,7 @@
  * Date: 2019/6/24
  * Time: 7:53
  */
-$http = new Swoole\Http\Server("127.0.0.1", 9501);
+$http = new Swoole\Http\Server("127.0.0.1", 8811);
 $http->on('request', function ($request, $response) {
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
