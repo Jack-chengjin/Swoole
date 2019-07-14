3a4b779ed0347d622faeda2c755de3800088aab5
lichengjin <a13381071993@163.com>
2019-07-14 17:57:08 +0800

    添加静态文件

--- Swoole/http.php
+++ Swoole/http.php
@@ -6,8 +6,13 @@
  * Time: 7:53
  */
 $http = new Swoole\Http\Server("0.0.0.0", 8811);
+$http -> set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => "/home/work/github/git-swoole/Swoole/Swoole/data"
+    ]
+);
 $http->on('request', function ($request, $response) {
-    print_r($request->get);
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 $http->start();
