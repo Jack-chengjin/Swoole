60759a65516622bb84ed3f09881db170e5859c39
lichengjin <a13381071993@163.com>
2019-07-14 17:39:23 +0800

    修改数据

--- Swoole/http.php
+++ Swoole/http.php
@@ -5,8 +5,9 @@
  * Date: 2019/6/24
  * Time: 7:53
  */
-$http = new Swoole\Http\Server("127.0.0.1", 8811);
+$http = new Swoole\Http\Server("0.0.0.0", 8811);
 $http->on('request', function ($request, $response) {
+    print_r($request->get);
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 $http->start();
