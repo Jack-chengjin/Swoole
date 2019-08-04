d763ed1f10e40c0ec8a98071007a3783471d27b5
lichengjin <a13381071993@163.com>
2019-08-04 19:06:18 +0800

    增加socket类

---
+++ Swoole/WS.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chengjin
+ * Date: 2019/8/4
+ * Time: 17:25
+ */
+
+class WS{
+    const HOST = '0.0.0.0';
+    const PORT = '8812';
+    public $ws = '';
+
+    public function __construct(){
+        $this->WS = new Swoole\WebSocket\Server('0,0,0,0', 8812);
+
+        $this->WS->on('open',[$this,'onOpen']);
+        $this->WS->on('message',[$this,'onMessage']);
+        $this->WS->on('close',[$this,'onClose']);
+    }
+
+    public function onOpen($server,$request){
+        var_dump($request->fd);
+    }
+
+    public function onMessage($server,$frame){
+        echo "server push message {$frame->data}\n";
+        $server->push($frame->fd,'server push:'.date("Y-m-d H:i:s"));
+    }
+
+
+    public function onClose($server,$fd){
+        echo "clientid:{$fd}";
+    }
+}
+
+$obj = new WS();
